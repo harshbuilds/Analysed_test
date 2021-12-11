1513c91dffd19ccdedcfb5a3a00a8efe997ce2df
Prity <63335298+Prity77@users.noreply.github.com>
2021-12-11 22:50:19 +0530

    Add files via upload
    Navigation bar for the recruiter and jobseeker signup pages

---
+++ signup/navBar.php
@@ -0,0 +1,16 @@
+<!-- NavBar-->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="./css/navBar.css">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href=""><img src="./img/logo.png" style="width:150px;height:53px;"></a>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <ul class="navbar-nav">
+       <li> <a class="nav-link" href="">Contact Us</a></li>
+        <li><a class="nav-link" href="">About</a></li>
+       <li> <a class="nav-link" href="">Login</a></li>
+	  </ul>
+    </div>
+  </div>
+</nav>
+<!--End of navBar-->
