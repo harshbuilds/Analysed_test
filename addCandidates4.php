4d916a4d679b5972674e92d0dbb25b2a9ba1b644
ashu-srm <78660556+ashu-srm@users.noreply.github.com>
2021-12-24 14:46:32 +0530

    Update addCandidates4.php

--- addCandidates4.php
+++ addCandidates4.php
@@ -1,22 +1,23 @@
+<!-- addCandidates4.php-->
 <?php
 
-   
+
     // $file_name = $_FILES['resume']['name'];
     // $file_size =$_FILES['resume']['size'];
     // $file_tmp =$_FILES['resume']['tmp_name'];
     // $file_type=$_FILES['resume']['type'];
     // $file_ext=strtolower(end(explode('.',$_FILES['resume']['name'])));
-    
+
    // $extensions= array(".docx",".doc",".pdf");
-    
+
     // if(in_array($file_ext,$extensions)=== false){
     //    $errors[]="extension not allowed, please choose a PDF or DOC file.";
     // }
-    
+
     // if($file_size > 6097152){
     //    $errors[]='File size must be less than 6 MB';
     // }
-    
+
     // if(empty($errors)==true){
     //    move_uploaded_file($file_tmp,"resume/".$file_name);
     //    echo "Success";
@@ -29,9 +30,9 @@
         {
             $_SESSION['info'][$key] = $value;
         }
-     
+
         $keys = array_keys($_SESSION['info']);
-     
+
         if(in_array('next', $keys)){
             unset($_SESSION['info']['next']);
         }
@@ -40,7 +41,7 @@
         extract($_SESSION['info']);
 
         $conn = mysqli_connect('localhost','root','','analysed');
-		
+
 		$date=date("Y-m-d");
 
         move_uploaded_file($_FILES["$image"]["tmp_name"],"img/".$_FILES["$image"]["name"]);
@@ -50,17 +51,17 @@
         $sql = mysqli_query($conn,"INSERT INTO candidates (image,firstname,lastname,candidate_email,countrycode,phone_no,birth_date,status,address,country,city_name,zipcode,state,current_title,company_name,candidate_type,emp_preference,source,recruiter,current_salary,salary_range1,salary_range2,talents,resume,skills,qualifications,comment,availability_date,job,accessibility,response_type,last_updated) VALUES ('$image','$firstname','$lastname','$emailid','$code','$phonenumber','$birthdate','$Industry','$address','$ownership','$cityname','$Zipcode','$State','$currentTitle','$companyName','$candidateType','$employmentPreference','$source','$recruiter','$currentSalary','$salaryRange1','$salaryRange2','$talents','$resume','$skills','$qualification','$candidateComment','$availability','$job','$accessebility','$reasonType','$date')");
 
         if($sql){
-			
+
             $_SESSION['firstname'] = $firstname;
 			header("Location: Candidates_specific.php");
              }
-             
+
              else{
                 echo mysqli_error($conn);
             }
         }
 
-     } 
+     }
 	 ?>
 <?php include('header1.php')?>
 <link rel="stylesheet" href="./css/addClient.css">
@@ -69,7 +70,7 @@
 <title>Analysed</title>
 <div class="container">
 <div class="bread-crumbs_Mytools-recruiter">
-        <a href="/" class="unactive-breadcrumb-link">Dashboard</a> > <a href="" class="unactive-breadcrumb-link">My Database</a> > <a href="" class="active-breadcrumb-link">Add candidates</a> 
+        <a href="js_dashboard.php" class="unactive-breadcrumb-link">Dashboard</a> > <a href="candidate_list.php" class="unactive-breadcrumb-link">My Database</a> > <a href="addCandidates4.php" class="active-breadcrumb-link">Add candidates</a>
     </div>
     <div class="small_container">
         <div class="heading_dash">
@@ -101,7 +102,7 @@
                                         <label for="default-input-for-no.1">Availability Date*</label>
                                         <input type="date" name="availability" class="default-input-for-add-client-1" placeholder="Company name" id="default-input-for-no.1" required />
                                     </p>
-                                    <p class="select-for-select-image-box-below-inputs  fx-city-name-1"> 
+                                    <p class="select-for-select-image-box-below-inputs  fx-city-name-1">
                                         <label for="default-select-for-no.6">Job*</label>
                                             <select name="job" id="default-select-for-no.6" class="default-select-for-add-client-1" required />
                                                 <option value="0" default>Select job</option>
@@ -110,7 +111,7 @@
                                                 <option value="UI Designer">UI Designer</option>
                                             </select>
                                     </p>
-                                    <p class="select-for-select-image-box-below-inputs  fx-city-name-1"> 
+                                    <p class="select-for-select-image-box-below-inputs  fx-city-name-1">
                                         <label for="default-select-for-no.6">Accessibility*</label>
                                             <select name="accessebility" id="default-select-for-no.6" class="default-select-for-add-client-1" required />
                                                 <option value="0" default>Select Accessiblility</option>
@@ -118,7 +119,7 @@
                                                 <option value="External">External</option>
                                             </select>
                                     </p>
-                                    <p class="select-for-select-image-box-below-inputs  fx-city-name-1"> 
+                                    <p class="select-for-select-image-box-below-inputs  fx-city-name-1">
                                         <label for="default-select-for-no.6">Response type*</label>
                                             <select name="reasonType" id="default-select-for-no.6" class="default-select-for-add-client-1" required />
                                                 <option value="0" default>Select response type</option>
