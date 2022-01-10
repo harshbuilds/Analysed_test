2543213af407009fa5e97a6bd99f01a64d193ed3
ashu-srm <78660556+ashu-srm@users.noreply.github.com>
2022-01-10 10:31:54 +0530

    Create recruiterLogIn.php

---
+++ css/recruiterLogIn.php
@@ -0,0 +1,122 @@
+* {
+    margin: 0px;
+    padding: 0px;
+  }
+
+  body {
+    background: white;
+  }
+  
+  .background img{
+      background-size:auto;
+      opacity:0.6;
+      
+  }
+
+  .background.navbar-nav {
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    font-size: 14px;
+    list-style: none;
+  
+  }
+
+  .navbar-nav li {
+    display: inline-block;
+    padding: 0px 20px;
+    position:relative;
+    right:-915;
+    top:-752;
+  }
+
+  .navbar-nav li a {
+    color: #3598db;
+    font-weight: 150;
+    text-decoration: none;
+  }
+
+  .navbar-nav li #contact {
+    color: white;
+    font-weight: 150;
+    text-decoration: none;
+  }
+ 
+  .loginbox{
+      width:380px;
+      height:420px;
+      background:white;
+      position:relative;
+      right:-700;
+      top:-440;
+      transform:translate(-50%,-50%);
+      box-sizing:border-box;
+      border-radius:20px;
+  }
+
+  .loginbox h3{
+      padding:0 0 20px 0;
+      position:relative; 
+      right:-160; 
+      top:30; 
+     
+  }
+
+  .txt-field input{
+    display:block;
+   position:relative;
+   top:70;
+   right:-50;
+   border: 1px solid #3598db;
+   border-radius: 5px;
+   background: #fafbff;
+   color: #3598db;
+   padding:10px;
+   margin-bottom: 30px;
+   width: 60%;
+   z-index:1000;
+ }
+
+.txt-field label{
+   position:relative;
+   top:60;
+   right:-50;
+}
+
+.pass{
+    position:relative;
+    top:60;
+    right:-50;
+    color:grey;
+}
+
+.button input{
+    position:relative;
+    top:100;
+    right:-150;
+    width:100;
+    padding:10px;
+    background-color:#3598db;
+    color:white;
+    border:none;
+    border-radius:6px;
+    cursor:pointer;
+}
+
+.button a{
+    position:relative;
+    top:140;
+    right:-71;
+    color:black;
+    text-decoration: none;
+    
+}
+
+.bot{
+    position:relative;
+    right:-790;
+    top:250;
+}
+
+
+
+
+
