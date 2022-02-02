6e47f8bf82ccc5453f462091dff7e2b66f1276d0
ashu-srm <78660556+ashu-srm@users.noreply.github.com>
2022-02-03 01:44:33 +0530

    Update addCandidates3.php

--- addCandidates3.php
+++ addCandidates3.php
@@ -1,22 +1,22 @@
-<!-- addCandidates3.php-->
 <?php
 
-    session_start();
+include 'conn.php';     //database connection page included
+session_start();      //session has been started
 
-    if(isset($_POST['next'])){
-        foreach ($_POST as $key => $value)
-        {
-            $_SESSION['info'][$key] = $value;
-        }
 
-        $keys = array_keys($_SESSION['info']);
+    if (isset($_POST['next'])) {
+      $_SESSION['current_title']  = $_POST['currentTitle'];
+      $_SESSION['company_Name'] = $_POST['companyName'];
+      $_SESSION['candidate_Type'] = $_POST['candidateType'];
+      $_SESSION['employment_Preference'] = $_POST['employmentPreference'];
+      $_SESSION['source'] = $_POST['source'];
+      $_SESSION['recruiter'] = $_POST['recruiter'];
+      $_SESSION['current_Salary'] = $_POST['currentSalary'];
+      $_SESSION['salary_Range1'] = $_POST['salaryRange1'];
+      $_SESSION['salary_Range2'] = $_POST['salaryRange2'];
 
-        if(in_array('next', $keys)){
-            unset($_SESSION['info']['next']);
-        }
-        header("Location: addCandidates4.php");
-     }
 
+    }
 ?>
 <?php include('header1.php')?>
 <link rel="stylesheet" href="./css/addClient.css">
@@ -26,7 +26,7 @@
 <title>Analysed</title>
 <div class="container">
     <div class="bread-crumbs_Mytools-recruiter">
-        <a href="js_dashboard.php" class="unactive-breadcrumb-link">Dashboard</a> > <a href="candidate_list.php" class="unactive-breadcrumb-link">My Database</a> > <a href="addCandidates3.php" class="active-breadcrumb-link">Add candidates</a>
+        <a href="/" class="unactive-breadcrumb-link">Dashboard</a> > <a href="" class="unactive-breadcrumb-link">My Database</a> > <a href="" class="active-breadcrumb-link">Add candidates</a>
     </div>
     <div class="small_container">
         <div class="heading_dash">
@@ -36,37 +36,46 @@
         <!-- <button class="save_button_addClient default-button-for-recruiter-dashboard">Save</button> -->
         <div class="row-recruiter container-for-add-client-main">
             <div class="left-side-row-for-add-client-container-1">
-                <p class="links-for-add-client-low-side-row-for-add-client active" id="link-categories-addClient-1"><a href="addCandidates1.php" >Personal infromation</a></p>
-                <p class="links-for-add-client-low-side-row-for-add-client" id="link-categories-addClient-2"><a href="addCandidates2.php">Professional infromation</a></p>
-                <p class="links-for-add-client-low-side-row-for-add-client" id="link-categories-addClient-3"><a href="addCandidates3.php"  class="active">Skills & Qualification</a></p>
-                <p class="links-for-add-client-low-side-row-for-add-client" id="link-categories-addClient-3"><a href="addCandidates4.php">Additional information</a></p>
+                <p class="links-for-add-client-low-side-row-for-add-client active" id="link-categories-addClient-1"><a href="edit-Candidates1.php" >Personal infromation</a></p>
+                <p class="links-for-add-client-low-side-row-for-add-client" id="link-categories-addClient-2"><a href="edit-Candidates2.php">Professional infromation</a></p>
+                <p class="links-for-add-client-low-side-row-for-add-client" id="link-categories-addClient-3"><a href="#"  class="active">Skills & Qualification</a></p>
+                <p class="links-for-add-client-low-side-row-for-add-client" id="link-categories-addClient-3"><a href="edit-Candidates4.php">Additional information</a></p>
             </div>
             <div class="right-side-row-for-add-client-container-1" id="general-information-addclient-1">
             <div class="row-recruiter" style="margin-bottom: 30px;">
                 <img src="./img/creativity.svg" width="50px" style="margin-right: 30px;">
                     <h2 class="heading-for-general-information-right-side-add-client-container">Skills & Qualification</h2>
                 </div>
-                <form action="" method="post">
+                <?php
+
+
+
+                                $sql=mysqli_query($con,"select * from candidates where candidate_id='".$_SESSION['cid']."'");
+                                $check=mysqli_num_rows($sql)>0;
+                                if($check){
+                                    while($row=mysqli_fetch_assoc($sql)){
+                       ?>
+                <form action="edit-Candidates4.php" method="post" enctype="multipart/form-data">
                 <div class="row-recruiter inputs-for-add-client-below-image-box">
                     <div class="right-side-image-box-right-add-client-1">
                                 <div class="row-recruiter sub-divs-image-box-right-side-add-client">
                                     <p class="input-para-add-client-ekam-1 talents">
                                         <label for="default-input-for-no.1">Talents*</label>
-                                        <input type="text" name="talents" class="default-input-for-add-client-1" placeholder="Company name" id="default-input-for-no.1" required />
+                                        <input type="text" name="talents" class="default-input-for-add-client-1" placeholder="Company name" id="default-input-for-no.1" value="<?php echo $row['talents']; ?>" required />
                                     </p>
                                     <div class="input-signup-div fx-city-name-1">
                                             <label for="file-upload ">Upload resume </label><br>
                                             <label for="file-upload"><span class="default-input-for-add-client-1">(*.docx, *.rtf, *.txt, *.pdf) <i class="fa fa-upload" aria-hidden="true"></i></span></label>
-                                            <input type="file" name="resume" id="file-upload" placeholder="docx" style="display:none;" required />
+                                            <input type="file" name="resume" id="file-upload" placeholder="docx" style="display:none;" value="<?php echo $row['resume']; ?>"/>
                                     </div>
                                     <p class="input-para-add-client-ekam-1 talents">
                                         <label for="default-input-for-no.1">Skills*</label>
-                                        <input type="text" name="skills" class="default-input-for-add-client-1" placeholder="Company name" id="default-input-for-no.1" required />
+                                        <input type="text" name="skills" class="default-input-for-add-client-1" placeholder="Company name" id="default-input-for-no.1" value="<?php echo $row['skills']; ?>" required />
                                     </p>
                                     <p class="input-para-add-client-ekam-1 right-side fx-city-name-1">
                                         <label for="default-select-for-no1 ">Qualification*</label>
                                         <select name="qualification" id="default-select-for-no.1" class="default-select-for-add-client-1" required />
-                                            <option value="0" default>Select candidates's degree</option>
+                                            <option value="<?php echo $row['qualification']; ?>" default><?php echo $row['qualification']; ?></option>
                                             <option value="MSc">MSc</option>
                                             <option value="BSc">BSc</option>
                                             <option value="BCom">B.Com</option>
@@ -76,6 +85,7 @@
                                 <input type = "submit" name = "next" class="save_button_addClient default-button-for-recruiter-dashboard" value="Save" >
                         </div>
                     </form>
+                <?php } } ?>
                 </div>
             </div>
         </div>
