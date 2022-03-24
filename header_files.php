e400e98114c2ba33c61915be086589e1b14ebaf9
khalidAthar9 <83690921+khalid96n@users.noreply.github.com>
2022-03-24 21:21:42 +0530

    Add files via upload

---
+++ header_files.php
@@ -0,0 +1,23 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<title>Analysed</title>
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!--FAVICON-->
+<meta name="description" content="" />
+<link href="img/mobiapp_logo_96dp.png" rel="apple-touch-icon" sizes="96x96">
+<link href="img/mobiapp_logo_96dp.png" rel="icon" sizes="96x96" type="image/png">
+<link href="img/mobiapp_logo_32dp.png" rel="icon" sizes="32x32" type="image/png">
+<meta content="img/mobiapp_logo_96dp.png" name="msapplication-TileImage">
+
+<!--FONT AWESOME LINKS-->
+<link rel='stylesheet' href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+
+<!--BOOSTRATP LINKS-->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+<!--JQUERY LINK-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
