f2cf492a6c377a7769b932a47d8269fa7d130703
Bansari Vora <79746509+Bansi09@users.noreply.github.com>
2022-04-05 15:30:01 +0530

    Add files via upload

---
+++ recruiter/includes/delete3.php
@@ -0,0 +1,11 @@
+<?php include('connection1.php');
+$id=$_GET['id'];
+$query="DELETE FROM files WHERE file_id='$id'";
+$result=mysqli_query($conn,$query);
+if($result){
+	header("Location: ../candidates_files.php");
+}
+else{
+	echo "<script>alert('Sorry,process failed')</script>";
+}
+?>
