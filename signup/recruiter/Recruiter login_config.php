00297354892c8cb4d604a938deca6cdc7f707618
Bansari Vora <79746509+Bansi09@users.noreply.github.com>
2022-05-16 20:40:58 +0530

    Update Recruiter login_config.php

--- signup/recruiter/Recruiter login_config.php
+++ signup/recruiter/Recruiter login_config.php
@@ -8,12 +8,13 @@ if(isset($_POST['submit']))
     $email=$_POST['email'];
     $password=$_POST['password'];
     $run_query=mysqli_query($conn,"SELECT * FROM recruiter WHERE email='$email'AND password='$password'");
-
+    $fetch =mysqli_fetch_assoc($run_query);    
     if(mysqli_num_rows($run_query))
     {
       // $_SESSION['firstname']=$firstname;
       // $_SESSION['recruiter_id']=$recruiter_id;
       $_SESSION['email']=$email;
+      $_SESSION['recruiter_id']= $fetch['recruiter_id'];
         echo "<script>window.open('./../../recruiter/dashboard.php','_self')</script>";
     }
     else
