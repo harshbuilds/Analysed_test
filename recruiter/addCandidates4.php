206ca25230a70b3d0889741f5c5b888064eddc6b
Bansari Vora <79746509+Bansi09@users.noreply.github.com>
2022-06-10 11:30:42 +0530

    Update addCandidates4.php

--- recruiter/addCandidates4.php
+++ recruiter/addCandidates4.php
@@ -32,77 +32,106 @@ session_start();            //session has been started
 
 
 <?php include('header.php')?>
-<link rel="stylesheet" href="./css/addClient.css">
+<!-- <link rel="stylesheet" href="./css/addClient.css"> -->
 <link rel="stylesheet" href="./css/addCandidates1.css">
-<!-- <link rel="stylesheet" href="../signup/recruiter/css/index.css"> -->
+<link rel="stylesheet" href="../signup/recruiter/css/index.css">
 <title>Analysed</title>
 <div class="container">
-<div class="bread-crumbs_Mytools-recruiter">
-        <a href="recruiter/dashboard.php" class="unactive-breadcrumb-link">Dashboard</a> > <a href="" class="unactive-breadcrumb-link">My Database</a> > <a href="" class="active-breadcrumb-link">Add candidates</a>
+    <div class="bread-crumbs_Mytools-recruiter">
+        <a href="recruiter/dashboard.php" class="unactive-breadcrumb-link">Dashboard</a> > <a href=""
+            class="unactive-breadcrumb-link">My Database</a> > <a href="" class="active-breadcrumb-link">Add
+            candidates</a>
     </div>
     <div class="small_container">
-        <div class="heading_dash">
-            <h1 class="mainHeadingDash">Add Candidate</h1>
-            <p class="mainParaDash">Please enter the details of the new candidates</p>
+        <div class="small_container_flex-div-1"
+            style="display:flex; justify-content:space-between; align-items:center;">
+            <div class=" heading_dash">
+                <h1 class="mainHeadingDash">Add Candidate</h1>
+                <p class="mainParaDash">Please enter the details of the new candidates</p>
+            </div>
+            <div class="heading_dash_save_button">
+                <input type="submit" name="next" class="save_button_addClient default-button-for-recruiter-dashboard"
+                    value="Save">
+                <img src="./img/save-icon.png" alt="" class="save_icon-img">
+            </div>
         </div>
         <!-- <button class="save_button_addClient default-button-for-recruiter-dashboard">Save</button> -->
-        <div class="row-recruiter container-for-add-client-main">
+        <div class="row-recruiter container-for-add-client-main" style="flex-wrap:nowrap; width:100%">
             <div class="left-side-row-for-add-client-container-1">
-                <p class="links-for-add-client-low-side-row-for-add-client active" id="link-categories-addClient-1"><a href="addCandidates1.php" >Personal infromation</a></p>
-                <p class="links-for-add-client-low-side-row-for-add-client" id="link-categories-addClient-2"><a href="addCandidates2.php">Professional infromation</a></p>
-                <p class="links-for-add-client-low-side-row-for-add-client" id="link-categories-addClient-3"><a href="addCandidates3.php">Skills & Qualification</a></p>
-                <p class="links-for-add-client-low-side-row-for-add-client" id="link-categories-addClient-3"><a href="addCandidates4.php" class="active">Additional information</a></p>
+                <p class="links-for-add-client-low-side-row-for-add-client active" id="link-categories-addClient-1"><a
+                        href="addCandidates1.php" class="inactive">Personal infromation</a></p>
+                <p class="links-for-add-client-low-side-row-for-add-client" id="link-categories-addClient-2"><a
+                        href="addCandidates2.php" class="inactive">Professional infromation</a></p>
+                <p class="links-for-add-client-low-side-row-for-add-client" id="link-categories-addClient-3"><a
+                        href="addCandidates3.php" class="inactive">Skills & Qualification</a></p>
+                <p class="links-for-add-client-low-side-row-for-add-client" id="link-categories-addClient-3"><a
+                        href="addCandidates4.php" class=" page-active">Additional information</a></p>
             </div>
             <div class="right-side-row-for-add-client-container-1" id="general-information-addclient-1">
-            <div class="row-recruiter" style="margin-bottom: 30px;">
-                <img src="./img/clipboard.svg" width="50px" style="margin-right: 30px;">
-                    <h2 class="heading-for-general-information-right-side-add-client-container">Additional information</h2>
+                <div class="row-recruiter sub-divs-image-child-main-heading" style="flex-wrap:nowrap;">
+                    <img src="./img/additional.png" width="70px" style="margin-right: 10px;">
+                    <h2
+                        class="heading-for-general-information-right-side-add-client-container row-recruiter-add-client-info-head">
+                        Additional information
+                    </h2>
                 </div>
-                <form action="addcandidateInsert.php" method="POST" enctype="multipart/form-data">   <!-- redirecting to insert query page with data of Additional information -->
-                <div class="row-recruiter inputs-for-add-client-below-image-box">
-                    <div class="right-side-image-box-right-add-client-1">
-                                <div class="row-recruiter sub-divs-image-box-right-side-add-client">
-                                    <p class="input-para-add-client-ekam-1">
-                                        <label for="default-input-for-no.1">Candidates Comments*</label>
-                                        <input type="text" name="candidateComment" class="default-input-for-add-client-1" placeholder="Company name" id="default-input-for-no.1" required />
-                                    </p>
-                                    <p class="input-para-add-client-ekam-1">
-                                        <label for="default-input-for-no.1">Availability Date*</label>
-                                        <input type="date" name="availability" class="default-input-for-add-client-1" placeholder="Company name" id="default-input-for-no.1" required />
-                                    </p>
-                                    <p class="select-for-select-image-box-below-inputs  fx-city-name-1">
-                                        <label for="default-select-for-no.6">Job*</label>
-                                            <select name="job" id="default-select-for-no.6" class="default-select-for-add-client-1" required />
-                                                <option value="0" default>Andhra Pradesh</option>
-                                                <option value="1">Open</option>
-                                                <option value="2">Closed</option>
-                                                <option value="3">Working</option>
-                                            </select>
-                                    </p>
-                                    <p class="select-for-select-image-box-below-inputs  fx-city-name-1">
-                                        <label for="default-select-for-no.6">Accessibility*</label>
-                                            <select name="accessebility" id="default-select-for-no.6" class="default-select-for-add-client-1" required />
-                                                <option value="0" default>Andhra Pradesh</option>
-                                                <option value="1">Open</option>
-                                                <option value="2">Closed</option>
-                                                <option value="3">Working</option>
-                                            </select>
-                                    </p>
-                                    <p class="select-for-select-image-box-below-inputs  fx-city-name-1">
-                                        <label for="default-select-for-no.6">Response type*</label>
-                                            <select name="reasonType" id="default-select-for-no.6" class="default-select-for-add-client-1" required />
-                                                <option value="0" default>Andhra Pradesh</option>
-                                                <option value="1">Open</option>
-                                                <option value="2">Closed</option>
-                                                <option value="3">Working</option>
-                                            </select>
-                                    </p>
-                                </div>
-                                <input type = "submit" name = "submit" class="save_button_addClient default-button-for-recruiter-dashboard" value="Submit" >
+                <form action="addcandidateInsert.php" method="POST" enctype="multipart/form-data">
+                    <!-- redirecting to insert query page with data of Additional information -->
+                    <div class="row-recruiter inputs-for-add-client-below-image-box">
+                        <div class="right-side-image-box-right-add-client-1">
+                            <div class="row-recruiter sub-divs-image-box-right-side-add-client">
+                                <p class="input-para-add-client-ekam-1 fb-2">
+                                    <label for="default-input-for-no.1">Candidates Comments*</label>
+                                    <input type="text" name="candidateComment" class="default-input-for-add-client-1"
+                                        placeholder="Enter comments" id="default-input-for-no.1" required />
+                                </p>
+                                <p class="input-para-add-client-ekam-1 fb-2">
+                                    <label for="default-input-for-no.1">Availability Date*</label>
+                                    <input type="date" name="availability" class="default-input-for-add-client-1"
+                                        placeholder="Company name" id="default-input-for-no.1" required />
+                                </p>
+                            </div>
+                            <div class="row-recruiter sub-divs-image-box-right-side-add-client">
+                                <p class="select-for-select-image-box-below-inputs  fx-city-name-1 fb-1">
+                                    <label for="default-select-for-no.6">Response type*</label>
+                                    <select name="reasonType" id="default-select-for-no.6"
+                                        class="default-select-for-add-client-1" required />
+                                    <option value="0" default>Internal</option>
+                                    <option value="1">Web Response</option>
+                                    <option value="2">Submission</option>
+                                    </select>
+                                </p>
+                            </div>
+                            <div class="row-recruiter sub-divs-image-box-right-side-add-client">
+                                <p class="select-for-select-image-box-below-inputs  fx-city-name-1 fb-2">
+                                    <label for="default-select-for-no.6">Job*</label>
+                                    <select name="job" id="default-select-for-no.6"
+                                        class="default-select-for-add-client-1" required />
+                                    <option value="0" default>Applying for which job</option>
+                                    <option value="1">Open</option>
+                                    <option value="2">Closed</option>
+                                    <option value="3">Working</option>
+                                    </select>
+                                </p>
+                                <p class="select-for-select-image-box-below-inputs  fx-city-name-1 fb-2">
+                                    <label for="default-select-for-no.6">Accessibility*</label>
+                                    <select name="accessebility" id="default-select-for-no.6"
+                                        class="default-select-for-add-client-1" required />
+                                    <option value="0" default>Internal</option>
+                                    <option value="1">Open</option>
+                                    <option value="2">Closed</option>
+                                    <option value="3">Working</option>
+                                    </select>
+                                </p>
+                            </div>
                         </div>
-                        </form>
+                        <!-- <input type="submit" name="submit"
+                            class="save_button_addClient default-button-for-recruiter-dashboard bottom-save-btn"
+                            value="Save"> -->
                     </div>
-                </div>
+                </form>
+            </div>
+        </div>
     </div>
-    </div>
+</div>
 </div>
